2883a8cd21a359acd0182d9ae58057a85ccae5ce
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-10-14 17:11:33 -0700

    5pm

--- app/controllers/life_hacks_controller.rb
+++ app/controllers/life_hacks_controller.rb
@@ -4,4 +4,13 @@ class LifeHacksController < ApplicationController
         render json: LifeHack.all
     end
 
+    # def create
+    #     new_hack = LifeHack.new(plug_params)
+    #     if hew_hack.save
+    #         render json: new_hack
+    #     else
+    #         render json: {new_hack.errors.full_messages}
+    #     end
+    # end    
+
 end
--- client/src/components/LifeHack.js
+++ client/src/components/LifeHack.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from 'react';
+//import { Button, NavItem } from 'react-bootstrap';
 
 function  LifeHack() {
     const [lifeHack, setLifeHack] = useState([]);
-    
+    const [title, setTitle] = useState('');    
+    const [kind, setKind] = useState('');
+    const [image, setImage] = useState('');
+    const [description, setDescription] = useState('');
 
     useEffect(() => {
         fetch("http://localhost:3000/life_hacks")
@@ -10,7 +14,64 @@ function  LifeHack() {
             .then(data => setLifeHack(data))
         }, []);
 
-    return (<div id="lifehack">
+    
+
+    function handleCreateLifeHack(e) {
+             console.log(`creating lifeHack..`);
+             e.preventDefault();
+    
+             const data = {
+                "lifeHack": {
+                     "title": title,
+                     "kind": kind,
+                     "image": image,
+                     "description": description
+                 }
+             }
+        
+             fetch("http://localhost:3000/life_hacks/", {
+               method: "POST",
+               headers: {
+                 'Content-Type': 'application/json'
+               },
+               body: JSON.stringify(data)
+             })
+               .then(response => response.json())
+               .then(lifeHacks => {
+                   console.log(`lifeHack created: ${JSON.stringify(lifeHacks)}`);
+                   setLifeHack([...lifeHack, lifeHacks]);
+               }) 
+        }
+
+        return (
+
+            
+        <div>  
+        <form className="note-editor">
+                  <label htmlFor='text'>
+                      Title:
+                      <input type= 'text' placeholder='insert title' onChange={(e) => setTitle(e.target.value)}  />
+                  </label>  
+                  <br/>
+                  <label htmlFor='text'>
+                      Kind:
+                      <input type= 'text' placeholder='insert kind' onChange={(e) => setKind(e.target.value)}  />
+                  </label>   
+                  <br/>  
+                  <label htmlFor='text'>
+                      Image:
+                      <input type= 'text' placeholder='insert image' onChange={(e) => setImage(e.target.value)}  />
+                  </label>
+                  <label htmlFor='text'>
+                      Description:
+                      <input type= 'text' placeholder='insert description' onChange={(e) => setImage(e.target.value)}  />
+                  </label>        
+                   <div className="button-row">
+                   <input onClick={handleCreateLifeHack} type="submit" value="Share Your Life Hack!"/>                          
+                      {/* <Button onClick={handleCreateLifeHack} variant="warning">Create LifeHack </Button>{' '} */}
+                  </div>            
+                </form>
+                 <div id="lifehack">
 
         <h1>Life Hacks!</h1>
         {lifeHack.map(item => 
@@ -28,6 +89,22 @@ function  LifeHack() {
         }
 
     </div>)
+            {/* <div className="gameLibrary">
+                {users.map((user, index) => {
+                    console.log(`user ${index}`);
+                    return (        
+                        <div key={user.updated_at}>
+                            <User user={user} users={users} setUsers={setUsers} />
+                        </div>
+                    )
+                })}
+        
+                
+             
+            </div> */}
+                   
+            </div>
+            )
 
 }
 
