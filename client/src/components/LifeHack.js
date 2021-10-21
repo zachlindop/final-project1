935bc91d843d983cf1819961efde7fe3f7acda7b
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-10-20 17:03:12 -0700

    2nav

--- client/src/components/LifeHack.js
+++ client/src/components/LifeHack.js
@@ -38,3 +38,30 @@ function  LifeHack({lifeHack, currentUserId}) {
 }
 
 export default LifeHack;
+
+// export default function App() {
+//     const [count, setCount] = useState(0);
+  
+//     useEffect(() => {
+//       setCount(JSON.parse(window.localStorage.getItem('count')));
+//     }, []);
+  
+//     useEffect(() => {
+//       window.localStorage.setItem('count', count);
+//     }, [count]);
+  
+//     const increaseCount = () => {
+//       return setCount(count + 1);
+//     }
+//     const decreaseCount = () => {
+//       return setCount(count - 1)
+//     }
+  
+//     return (
+//       <div className="App">
+//         <h1> Count {count} </h1>
+//         <button onClick={increaseCount}>+</button>
+//         <button onClick={decreaseCount}>-</button>
+//       </div>
+//     );
+//   }
